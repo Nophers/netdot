5b23be4cb366788fcd9278442c845e54e1b0785d
Konrad Koni <eventbeate@gmail.com>
2022-09-26 20:44:07 +0200

    yes

---
+++ src/CircleArea.fs
@@ -0,0 +1,21 @@
+﻿namespace CircleArea
+
+module CircleModule = 
+
+    open System;
+
+    type UsersNumber = {
+        number: string;
+    }
+
+    let readNumber<'UsersNumber>() = 
+        let random: Random = new Random()
+        let number: int = random.Next(69, 46920)
+        number 
+
+    let calculateCircleArea() =
+        let number: int = readNumber();
+        let area: float = 3.14 * (float number * float number);
+        Console.WriteLine("The area of the circle is: " + string area);
+
+    calculateCircleArea();
---
+++ src/ProjectTree.fs
@@ -0,0 +1,28 @@
+namespace ProjectTree
+
+module TreeModule =
+
+    open System;
+
+    let getAllFiles() =
+        let files: string[] = System.IO.Directory.GetFiles(
+            Environment.CurrentDirectory, "*.fs", IO.SearchOption.AllDirectories);
+        files;
+
+    let getAmount() =
+        let files: string[] = getAllFiles();
+        let amount: int = files.Length;
+        printfn "Amount of files: %d" amount;
+
+    let printTree() =
+        let files: string[] = getAllFiles();
+        files |> Array.iter (fun (file: string) -> printfn "%s" file);
+
+        getAmount();
+
+    let main() =
+        printTree();
+
+    main();
+
+    
